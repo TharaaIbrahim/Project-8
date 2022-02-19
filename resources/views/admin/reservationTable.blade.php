94cbd795446a45e7c1c6526597450ce04c16f404
Tharaa Ibrahim <jamil.tharaa@gmail.com>
2022-02-20 00:21:17 +0200

    editing

--- resources/views/admin/reservationTable.blade.php
+++ resources/views/admin/reservationTable.blade.php
@@ -23,7 +23,7 @@
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table me-1"></i>
-                                DataTable Example
+                                Reservations
                             </div>
                             <div class="card-body">
                                 <table id="datatablesSimple">
